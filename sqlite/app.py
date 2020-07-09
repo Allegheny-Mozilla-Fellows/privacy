79abc24cb64ed5832b8b84867699c92d7f8542e7
Jordan Wilson <wilsonj3@allegheny.edu>
2020-07-09 12:11:22 -0400

    Add changes to webserver

--- sqlite/app.py
+++ sqlite/app.py
@@ -13,18 +13,28 @@ def log():
     g.db = connect_db()
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    #    username = request.form[username]
+
+        cur = g.db.execute("select username from managers where username=\""+request.form['username']+"\""+"and password=\""+request.form['password']+"\"")
+
+        username=cur.fetchone()
+        if username is None:
             error = 'Invalid Credentials. Please try again'
+            return render_template('log.html', error=error)
         else:
             return redirect(url_for('hello'))
-    return render_template('log.html', error=error)
+    else:
+        return render_template('log.html', error=error)
+
+
+#cur = g.db.execute('select username from managers where username=‘+’request.form[‘username’]’+’and password=‘+‘ request.form[‘password’]')
 
 
 @app.route('/hello')
 def hello():
     g.db = connect_db()
     cur = g.db.execute('select * from Customers')
-    Customers = [dict(id=row[0], firstname=row[1], lastname=row[2], age=row[3], street=row[4], city=row[5], state=row[6], zip=row[7], salary=row[8], eyecolor=row[9], dob=row[10], religion=row[11], politicalaffiliation=row[12], relationshipstatus=row[13]) for row in cur.fetchall()]
+    Customers = [dict(id=row[0], firstname=row[1], lastname=row[2], age=row[3], street=row[4], city=row[5], state=row[6], zip=row[7], salary=row[8], eyecolor=row[9], dob=row[10], religion=row[11], politicalaffiliation=row[12], relationshipstatus=row[13], ethnicity=row[14]) for row in cur.fetchall()]
     g.db.close()
     return render_template('hello.html', Customers=Customers)
 
